ed47f5f1a75c30e3ffa4de862bfcab3afe2ad330
13949836783@163.com <13949836783@163.com>
2017-07-26 22:50:29 +0800

    add image to product

--- config/application.rb
+++ config/application.rb
@@ -12,4 +12,6 @@ module Lujun
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+  require 'carrierwave'
+require 'carrierwave/orm/activerecord'
 end
