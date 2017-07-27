e9abe64832e75d8d83187b15bbb82cb7d8361c9b
13949836783@163.com <13949836783@163.com>
2017-07-27 12:30:35 +0800

    user cannnot add same product to cart twice

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,11 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
-    flash[:notice] = "成功将商品加入购物车"
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "成功将商品加入购物车"
+    else
+      flash[:warning] = "购物车内已有此物品"
     redirect_to product_path(@product)
   end
 
